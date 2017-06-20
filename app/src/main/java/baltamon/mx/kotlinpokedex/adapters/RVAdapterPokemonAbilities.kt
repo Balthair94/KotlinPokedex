7c461fbc1deea75ccac4588a393a03094471554e
Sinuhe Jaime <sinuhe_jaime@epam.com>
2017-06-20 17:25:14 -0500

    Added extension function that involves Adapters

--- app/src/main/java/baltamon/mx/kotlinpokedex/adapters/RVAdapterPokemonAbilities.kt
+++ app/src/main/java/baltamon/mx/kotlinpokedex/adapters/RVAdapterPokemonAbilities.kt
@@ -1,50 +1,39 @@
 package baltamon.mx.kotlinpokedex.adapters
 
 import android.support.v4.app.FragmentManager
-import android.content.Context
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import baltamon.mx.kotlinpokedex.R
+import baltamon.mx.kotlinpokedex.extension.inflate
 import baltamon.mx.kotlinpokedex.fragments.AbilityDialogFragment
 import baltamon.mx.kotlinpokedex.models.NamedAPIResource
-import baltamon.mx.kotlinpokedex.models.PokemonAbility
 import kotlinx.android.synthetic.main.item_title.view.*
 
 /**
  * Created by Baltazar Rodriguez on 13/06/2017.
  */
-class RVAdapterPokemonAbilities internal constructor(internal var abilities: ArrayList<NamedAPIResource>,
-                                                     internal var context: Context,
-                                                     internal var fragmentManager: FragmentManager) :
+class RVAdapterPokemonAbilities(val abilities: ArrayList<NamedAPIResource>,
+                                val fragmentManager: FragmentManager) :
         RecyclerView.Adapter<RVAdapterPokemonAbilities.AbilityViewHolder>() {
 
-    class AbilityViewHolder internal constructor(itemView: View): RecyclerView.ViewHolder(itemView){
-        internal var abilityName = itemView.tv_title_item
-        internal var cardView = itemView.card_view
+    class AbilityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvAbilityName: TextView = itemView.tv_title_item
+        val cardView: CardView = itemView.card_view
     }
 
-    override fun getItemCount(): Int {
-        return abilities.size
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AbilityViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_title, parent, false)
-        val pvh = RVAdapterPokemonAbilities.AbilityViewHolder(v)
-        return pvh
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AbilityViewHolder =
+            RVAdapterPokemonAbilities.AbilityViewHolder(parent.inflate(R.layout.item_title))
 
     override fun onBindViewHolder(holder: AbilityViewHolder, position: Int) {
-        holder.abilityName.text = abilities[position].name
+        holder.tvAbilityName.text = abilities[position].name
         holder.cardView.setOnClickListener {
-            val dialogFragment = AbilityDialogFragment().
-                    newInstance(abilities[position])
+            val dialogFragment = AbilityDialogFragment().newInstance(abilities[position])
             dialogFragment.show(fragmentManager, "Detail")
         }
     }
 
-    override fun onAttachedToRecyclerView(recyclerView: RecyclerView?) {
-        super.onAttachedToRecyclerView(recyclerView)
-    }
+    override fun getItemCount(): Int = abilities.size
 }
