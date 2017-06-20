e84f3f9e3066831cb0573bc7db57477a96e13bf6
Sinuhe Jaime <sinuhe_jaime@epam.com>
2017-06-20 18:19:22 -0500

    Fixed another fragment

--- app/src/main/java/baltamon/mx/kotlinpokedex/fragments/PokemonAboutFragment.kt
+++ app/src/main/java/baltamon/mx/kotlinpokedex/fragments/PokemonAboutFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import baltamon.mx.kotlinpokedex.R
 import baltamon.mx.kotlinpokedex.models.Pokemon
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_pokemon_about.*
 import kotlinx.android.synthetic.main.fragment_pokemon_about.view.*
 
 /**
@@ -30,26 +31,29 @@ class PokemonAboutFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_pokemon_about, container, false)
-        showDetails(view)
-        return view
+        return inflater.inflate(R.layout.fragment_pokemon_about, container, false)
     }
 
-    fun showDetails(view: View) {
-        val pokemon: Pokemon = arguments.getParcelable<Parcelable>(MY_OBJECT_KEY) as Pokemon
-        view.tv_pokemon_name.text = pokemon.name
-        view.tv_weight.text = "Weight: ${pokemon.weight}"
-        view.tv_hight.text = "Hight: ${pokemon.height}"
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showDetails()
+    }
 
-        Picasso.with(context).load(pokemon.sprites.front_default).into(view.iv_pokemon_image)
-        Picasso.with(context).load(pokemon.sprites.front_default).into(view.iv_form1)
-        Picasso.with(context).load(pokemon.sprites.back_default).into(view.iv_form2)
+    fun showDetails() {
+        val pokemon: Pokemon = arguments.getParcelable<Parcelable>(MY_OBJECT_KEY) as Pokemon
+        tv_pokemon_name.text = pokemon.name
+        tv_weight.text = "Weight: ${pokemon.weight}"
+        tv_hight.text = "Hight: ${pokemon.height}"
+
+        Picasso.with(context).load(pokemon.sprites.front_default).into(iv_pokemon_image)
+        Picasso.with(context).load(pokemon.sprites.front_default).into(iv_form1)
+        Picasso.with(context).load(pokemon.sprites.back_default).into(iv_form2)
 
         var textTypes = "Types: "
         for (item in pokemon.types) {
             textTypes += item.type.name + ", "
         }
-        view.tv_types.text = textTypes
+        tv_types.text = textTypes
     }
 
 }
